09014c0dbdc661845f7252ed60e7e46f044e9616
cheng guo <c43guo@uwaterloo.ca>
2016-10-15 17:53:47 -0400

    binary search practice

--- linklist.py
+++ linklist.py
@@ -4,6 +4,9 @@ class Node:
         self.next = None
         self.data = data
 
+    def __str__(self):
+        return self.data
+
     def getData(self):
         return self.data
 
@@ -14,6 +17,10 @@ class Linklist:
   
     def __init__(self):
         self.head = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
 
     def setHead(self, newhead):
         self.head = newhead
@@ -27,37 +34,63 @@ class Linklist:
             while current.next:
                 current = current.next
 
-            current.next = temp
+            current.next = temp 
+            self.size +=1
     def search(self, data):
         current = self.head
         if current is None:
             print 'empty link list'
+        elif current.next is None:
+            return current.data == data
         else:
             while current:
                 if current.data == data:
                     return True
                 current = current.next
         return False
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        if current is None:
+            return False
+ 
+        while current:
+            tmp = current.next
+            current.next = prev
+            prev = current
+            current = tmp
             
-                
+
+        self.head = prev
+        return True
 
     def printList(self):
         current = self.head
         while current:
-            print (current.data + '->'),;
+            print current 
             current = current.next
 
+        print ''
+
 linklist = Linklist()
+
 linklist.addNode('a')
+if linklist.search('a') is False:
+    print 'asdasd'
+else:
+    print 'haha'
 linklist.addNode('b')
 linklist.addNode('c')
 linklist.addNode('d')
-if linklist.search('c'):
-    print 'found it'
-else:
-    print 'does not exist'
+linklist.addNode('e')
+linklist.addNode('f')
 
-linklist.printList()
+
+
+
+
+linklist.reverse()
 
 
         
--- loop.py
+++ loop.py
@@ -1,11 +1,21 @@
-haha = [3,2,4,1,2,5,2]
 
-# for i in haha:
-#     if len(str(i)) > 1:
-#         print i
-#     
+def binarySearch (sorted,target,left,right):
+    
 
+    mid = (right - left)/2 + left
+    print(sorted[mid])
 
-asd = 'abcdefg'
+    if sorted[mid] == target:
+        return True
+    elif sorted[mid] < target and left < right:
+        return binarySearch(sorted,target, mid+1, right)
+    elif sorted[mid] > target and left < right:
+        return binarySearch(sorted,target, left, mid-1)
 
+    return False
 
+haha = range(1,101)
+if binarySearch(haha,1,0,len(haha)-1):
+    print('found it')
+else:
+    print('did not found the number ')
