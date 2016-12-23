a6404320770f144d474e84db86e56ee566459774
cheng guo <c43guo@uwaterloo.ca>
2016-12-23 15:36:29 -0500

    update

---  Chapter 1 Array and String/1.2 checkPermutation.py
+++  Chapter 1 Array and String/1.2 checkPermutation.py
@@ -1,13 +1,14 @@
 # given two string check if one is a permutation of the other
 
 def solution(str_one,str_two):
+    str_one.replace(' ','')
+    str_two.replace(' ','')
     if len(str_one) != len(str_two):
         return False
     if len(str_one) == 0 or len(str_one) == 1:
         return str_one == str_two
     if (sorted(str_one) == sorted(str_two)):
         return True
-
     return False
 
 
