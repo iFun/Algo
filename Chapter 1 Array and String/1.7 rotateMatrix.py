7eb357169c8627b00292cc82006289101dcb0c84
cheng guo <c43guo@uwaterloo.ca>
2016-12-31 23:12:27 -0500

    1.7

--- Chapter 1 Array and String/1.7 rotateMatrix.py
+++ Chapter 1 Array and String/1.7 rotateMatrix.py
@@ -1,9 +1,41 @@
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
 
 def solution():
+    
+
+    for layer in xrange(0,len(matrix)):
+        first = layer
+        last = len(matrix) - 1 - first
+        for i in xrange(first,last):
+            offset = i - first
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset]= matrix[i][last]
+            matrix[i][last] = top
+            
+
+    printMatrix()
+    return matrix
+
+def printMatrix():
+    print matrix[0]
+    print matrix[1]
+    print matrix[2]
+    print matrix[3]
+    print ''
 
 
+def main():
+    rotatedMatrix = solution()
+    assert (rotatedMatrix[0] == [13,9,5,1]),"wrong"
+    assert (rotatedMatrix[1] == [14,10,6,2]),"wrong"
+    assert (rotatedMatrix[2] == [15,11,7,3]),"wrong"
+    assert (rotatedMatrix[3] == [16,12,8,4]),"wrong"
 
+    print 'all passed'
 
 
 if __name__ == '__main__':
-    solution()
+    main()
