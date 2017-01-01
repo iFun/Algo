df234f8be5eb93ec451354fc11382edb8302c1ca
cheng guo <c43guo@uwaterloo.ca>
2016-12-31 23:36:46 -0500

    optimize

--- Chapter 1 Array and String/1.8 zeroMatrix.py
+++ Chapter 1 Array and String/1.8 zeroMatrix.py
@@ -2,26 +2,20 @@ matrix = [[0,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
 
 
 def solution():
-    row = [None] * len(matrix)
-    col = [None] * len(matrix[0])
-    counter = 0
+    row = []
+    col = []
+
     for i in xrange(0,len(matrix)):
         for j in xrange(0,len(matrix[0])):
-            if matrix[i][j] == 0:
-                
-                row[counter] = i
-                col[counter] = j
-                counter += 1
+            if matrix[i][j] == 0:     
+                row.append(i)
+                col.append(j)
 
     for x in xrange(0,len(row)):
-        if row[x] == None:
-            break
         for j in xrange(0,len(matrix[0])):
             matrix[row[x]][j] = 0
 
     for x in xrange(0,len(col)):
-        if col[x] == None:
-            break
         for j in xrange(0,len(matrix)):
             matrix[j][col[x]] = 0
             
