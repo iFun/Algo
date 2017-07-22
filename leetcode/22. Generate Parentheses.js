b92e76e82d31812e432e109f9eba5b6a05ed891f
Bill Guo <iFun@users.noreply.github.com>
2017-07-22 02:24:58 -0400

    Create 22. Generate Parentheses.js

---
+++ leetcode/22. Generate Parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    var result = [];
+    backtrack(0,0,"",n,result);
+    return result;
+};
+
+function backtrack (open,close,str,max, result){
+    if(str.length == max*2){
+        result.push(str);
+        return;
+    }
+    
+    if(open < max){
+        generate(open+1, close, str+"(", max, result);
+    }
+    if(close < open){
+        generate(open, close+1, str+")", max, result);
+    }
+    return;
+}
