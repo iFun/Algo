dc716f9b24915d605b478ae83596bae96f3c6bd4
Bill Guo <iFun@users.noreply.github.com>
2017-07-22 02:30:12 -0400

    update comment

--- leetcode/22. Generate Parentheses.js
+++ leetcode/22. Generate Parentheses.js
@@ -9,14 +9,17 @@ var generateParenthesis = function(n) {
 };
 
 function backtrack (open,close,str,max, result){
+    //total is n*2
     if(str.length == max*2){
         result.push(str);
         return;
     }
     
+    //add as much as (
     if(open < max){
         backtrack(open+1, close, str+"(", max, result);
     }
+    //after adding ( start to add ), but if close number is = or > then open stop adding
     if(close < open){
         backtrack(open, close+1, str+")", max, result);
     }
