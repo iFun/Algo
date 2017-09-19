7f16855d633682e6e939e3162ca3caeb0acc38b0
Bill Guo <iFun@users.noreply.github.com>
2017-09-19 10:23:09 -0400

    Create 48. Rotate Image.js

---
+++ leetcode/48. Rotate Image.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    matrix.reverse()
+    for(let i = 0; i< matrix.length; i++){
+        for(let j = i + 1; j < matrix[0].length; j++){
+            let tmp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = tmp
+        }
+    }
+};
